577e074a5037984e640b32578881e6935c0de3a9
GDaniel <gerardo.ladera@unmsm.edu.pe>
2024-06-19 22:09:45 -0500

    Proyecto estructurado

---
+++ inventarioComponentesBackend/src/main/java/inventarioComponentesBackend/ProyectoInventarioDeComponentesBackendApplication.java
@@ -0,0 +1,13 @@
+package inventarioComponentesBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoInventarioDeComponentesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoInventarioDeComponentesBackendApplication.class, args);
+	}
+
+}
---
+++ inventarioComponentesBackend/src/test/java/inventarioComponentesBackend/ProyectoInventarioDeComponentesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package inventarioComponentesBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoInventarioDeComponentesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
